0583c956f2f9e5275d83d70601baff5cefbcbe7c
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-08 15:08:50 -0800

    entry actions and util

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h2>POKEMON AAAUGH</h2>, root);
+  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
 });
